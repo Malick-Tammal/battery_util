5fcdf4c4a1adcd82f4a0194425625fbde1eb09ff
ADAMSKI-DZ <malick.tammal15@gmail.com>
2024-04-24 12:35:37 +0100

    Small changes

--- test/test.js
+++ test/test.js
@@ -42,7 +42,7 @@ const getBatteryData = () => {
     );
     const health = `${calcBatteryHealth(fullChargeCapacity, designCapacity)}%`;
     const cycleCount = util.getValue(dataSplitted, "cycleCount", ":");
-    const batteryId = util.getValue(dataSplitted, "id", ":");
+    const id = util.getValue(dataSplitted, "id", ":");
     const serialNumber = util.getValue(dataSplitted, "serialNumber", ":");
 
     const allData = {
@@ -52,7 +52,7 @@ const getBatteryData = () => {
       fullChargeCapacity: fullChargeCapacity,
       health: health,
       cycleCount: cycleCount,
-      batteryId: batteryId,
+      id: id,
       serialNumber: serialNumber,
     };
 
